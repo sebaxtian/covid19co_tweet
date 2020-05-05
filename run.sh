79bbb510695ee9880089812fcf04e2392df71d6f
Sebastian Rios Sabogal <sebaxtianrioss@gmail.com>
2020-05-04 23:23:56 -0500

    Kaggle integration

--- run.sh
+++ run.sh
@@ -4,4 +4,17 @@
 # Run covid19co_tweet.py Python script
 #
 
+# Local .env
+if [ -f .env ]; then
+    # Load Environment Variables
+    export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+    # For instance
+    #echo $KAGGLE_KEY
+fi
+
+# Download Kaggle Dataset
+if [ $KAGGLE_KEY ] && [ $KAGGLE_USERNAME ]; then
+    kaggle datasets download -d sebaxtian/covid19co -p ./kaggle --unzip
+fi
+
 echo "Work in progress ..."
